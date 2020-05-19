029081f892f8b1853739b1960c2d2aa9e9b19f05
yelee <ye.lee.ds@kt.com>
2020-05-19 09:13:22 +0900

    190519 member mapper edited

--- myproject/src/main/java/com/ktds/devpro/model/mapper/MemberMapper.java
+++ myproject/src/main/java/com/ktds/devpro/model/mapper/MemberMapper.java
@@ -1,5 +1,7 @@
 package com.ktds.devpro.model.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ktds.devpro.model.vo.Member;
@@ -8,5 +10,5 @@ import com.ktds.devpro.model.vo.MemberVO;
 @Mapper
 public interface MemberMapper {
 	public MemberVO findMemberById(String id);
-	public Member selectMemberTest();
+	public List<Member> selectMemberTest();
 }
