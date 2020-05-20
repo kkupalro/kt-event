7fdcaa85e6bb4d9b42fd2fd0d0d15e7ac68b1bc4
taejun.yoo <taejun.yoo@kt.com>
2020-05-20 13:57:49 +0900

    유태준 | merge

--- myproject/bin/member.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE member(
-	id VARCHAR(100) PRIMARY KEY,
-	password VARCHAR(100) NOT NULL,
-	name VARCHAR(100) NOT NULL,
-	address VARCHAR(100) NOT NULL	
-);
-
-INSERT INTO member(id,password,name,address) VALUES('java','1234','아이유','종로');
-COMMIT;
